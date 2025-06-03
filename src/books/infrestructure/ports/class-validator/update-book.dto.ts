c3cf4563ad247f17207b3a80060e19c43b016ba6
emma <223729@ids.upchiapas.edu.mx>
2025-06-02 18:39:01 -0600

    cambios

--- src/books/infrestructure/ports/class-validator/update-book.dto.ts
+++ src/books/infrestructure/ports/class-validator/update-book.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateBookDto } from './create-book.dto';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(OmitType(CreateBookDto, ['id_user'] as const)) {}
